bbda84aaeae90172006d5242a8ef73c070e32871
joshbean3 <joshdbean3@gmail.com>
2025-06-19 16:30:20 -0400

    build out initial functionality of game Built out the basic functionality for rps, next, I need to build a way to track scores globally and continue the game

--- java-for-rps.js
+++ java-for-rps.js
@@ -1,9 +1,9 @@
 
 function getComputerChoice() {
     // assign a number to rock, paper, and scissors
-    let rock = 1
-    let paper = 2
-    let scissors = 3
+    let rock = 0
+    let paper = 1
+    let scissors = 2
     // get a random number from random number generator
     function getRandomInt(max) {
         return Math.floor(Math.random() * max);
@@ -22,3 +22,59 @@ function getComputerChoice() {
     }
 }
 
+
+function getHumanChoice() {
+    let human = prompt("rock, paper, or scissors?").toLowerCase();
+    return human;
+}
+
+
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(computerChoice);
+console.log(humanChoice);
+playRound(computerChoice, humanChoice);
+
+function playRound(computerChoice, humanChoice) {
+    // Compare computerChoice to HumanChoice using OR operators
+    if (humanChoice === "rock"){
+        if (computerChoice === "scissors"){
+            humanScore += 1;
+            console.log("Human 1 wins!");
+            return humanScore;
+        } else if (computerChoice === "paper") {
+            computerScore += 1;
+            console.log("computer 1 wins!");
+            return computerScore;
+        }else if (computerChoice === "rock"){
+            console.log("It is a tie! Play again")
+        }
+    } else if (humanChoice === "paper"){
+        if (computerChoice === "rock"){
+            humanScore += 1;
+            console.log("Human 1 wins!");
+            return humanScore;
+        } else if (computerChoice === "scissors") {
+            computerScore += 1;
+            console.log("computer 1 wins!");
+            return computerScore;
+        }else if (computerChoice === "paper"){
+            console.log("It is a tie! Play again")
+        }
+    } else if (humanChoice === "scissors "){
+        if (computerChoice === "paper"){
+            humanScore += 1;
+            return humanScore;
+            console.log("Human 1 wins!");
+        } else if (computerChoice === "rock") {
+            computerScore += 1;
+            return computerScore;
+            console.log("computer 1 wins!");
+        }else if (computerChoice === "scissors"){
+            console.log("It is a tie! Play again")
+        }
+    }
+}
